f9fe8658de4ee918d205e787a31402941f2c6359
sasi <sasi.muralitechlab@gmail.com>
2019-07-26 18:18:26 +0530

    latest changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'qrcode-generator';
+  elementType : 'url' | 'canvas' | 'img' = 'url';
+value : string = 'Techiediaries';
+}
